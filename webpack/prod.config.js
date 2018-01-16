6056d76916206e60e67347253a68ed9a241941d0
stavl <stavlev95@gmail.com>
2018-01-16 20:04:27 +0200

    fix(webpack-bundle-js): Fixed build on index.css by configuring prod.config.js

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -22,8 +22,24 @@ module.exports = {
                 exclude: [path.resolve(__dirname, "node_modules")],
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader']
+                    use: ['css-loader', 'sass-loader'],
                 })
+            },
+            {
+                // Transform our own .css files with PostCSS and CSS-modules
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                // Do not transform vendor's CSS with CSS-modules
+                // The point is that they remain in global scope.
+                // Since we require these CSS files in our JS or CSS files,
+                // they will be a part of our compilation either way.
+                // So, no need for ExtractTextPlugin here.
+                test: /\.css$/,
+                include: /node_modules/,
+                use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.svg$/,
