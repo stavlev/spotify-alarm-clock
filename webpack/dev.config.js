07bc286baa4d3a43129b0824157dd3c4fb2c098c
stavl <stavlev95@gmail.com>
2018-01-18 22:08:39 +0200

    fix(webpack-dev-config): Fixed paths in dev.config.js so that bundle.js would fully display our client-side code using source maps

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,49 +1,98 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    'babel-polyfill',
-    path.join(__dirname, '../client/index'),
-  ],
-  output: {
-    path: path.join(__dirname, '../public/'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  module: {
-    loaders: [
-      { test: /\.svg$/, loaders: ['raw-loader']},
-      // take all less files, compile them, and bundle them in with our js bundle
-      { test: /\.less$/, loader: 'style!css!autoprefixer?browsers=last 2 version!less' },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react'],
-          plugins: [['react-transform', {
-            transforms: [{
-              transform: 'react-transform-hmr',
-              imports: ['react'],
-              // this is important for Webpack HMR:
-              locals: ['module']
-            }],
-          }]],
-        },
-      },
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        'babel-polyfill',
+        path.join(__dirname, '../client/src/index.js'),
+        path.join(__dirname, '../client/src/styles/index.scss'),
     ],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('development'),
-      },
-    }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-  ],
+    output: {
+        path: path.join(__dirname, '../public/'),
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                // take all scss files, compile them, and bundle them in with our js bundle
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader'],
+                })
+            },
+            {
+                // Transform our own .css files with PostCSS and CSS-modules
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                // Do not transform vendor's CSS with CSS-modules
+                // The point is that they remain in global scope.
+                // Since we require these CSS files in our JS or CSS files,
+                // they will be a part of our compilation either way.
+                // So, no need for ExtractTextPlugin here.
+                test: /\.css$/,
+                include: /node_modules/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.svg$/,
+                loader: 'raw-loader',
+            },
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react'],
+                    plugins: [['react-transform', {
+                        transforms: [{
+                            transform: 'react-transform-hmr',
+                            imports: ['react'],
+                            // this is important for Webpack HMR:
+                            locals: ['module']
+                        }],
+                    }]],
+                },
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react', 'stage-0'],
+                    plugins: [['react-transform', {
+                        transforms: [{
+                            transform: 'react-transform-hmr',
+                            imports: ['react'],
+                            // this is important for Webpack HMR:
+                            locals: ['module']
+                        }],
+                    }]],
+                },
+            },
+        ],
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('development'),
+            },
+        }),
+        // optimizations
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new ExtractTextPlugin({
+            filename: '[name].bundle.css',
+            disable: false,
+            allChunks: true,
+        }),
+    ],
 };
